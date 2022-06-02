f1bba8566f8e0b08ef6f5d1ab69c81748ef60cbb
Tvoir <TommyVoir@yahoo.com>
2022-06-01 19:15:26 -0700

    changed the footer text color to blue

--- index.js
+++ index.js
@@ -3,3 +3,19 @@
 
 let header = document.querySelector('#page-header');
 header.style.textAlign ='left';
+
+let dogPics = document.querySelectorAll('.dog-image');
+for(let i = 0; i < dogPics.length; i++){
+    dogPics[i].style.borderRadius = '50px'
+}
+
+let dogName = document.querySelectorAll('.dog-name')
+for(let i = 0; i <dogName.length; i++){
+dogName[i].style.textAlign ='left'
+}
+
+let footer = document.querySelector('.footer');
+footer.style.color ='Blue';
+
+
+
